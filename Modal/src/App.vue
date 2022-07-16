b7d6b7d530e593ff50faeaf27d7e1f37a744a570
Likhaya-coder <60583056+Likhaya-coder@users.noreply.github.com>
2022-07-16 23:48:45 +0200

    Add files via upload

---
+++ Modal/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+   <p>Welcome...</p>
+   <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+   </div>
+   <button class="btn" @click="toggleModal">Open Modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      header: 'Sign up for the giveaway',
+      text: 'Grab your ninja price for half price promo',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  .btn {
+    padding: 15px 25px;
+    background-color: rgb(4, 109, 56);
+    border: 0;
+    color: #fff;
+    font-size: 24px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+</style>
---
+++ Modal/src/components/Modal.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="overlay" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme==='sale'}">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .overlay {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.6);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+    }
+    .overlay .modal {
+        width: 500px;
+        height: 200px;
+        background-color: #fff;
+        justify-content: center;
+        align-items: center;
+        cursor: default;
+        border-radius: 10px;
+    }
+    
+    .overlay .modal h1 {
+        padding-top: 30px;
+        font-weight: 800;
+        font-size: 36px;
+        color: rgb(2, 235, 118);
+    }
+
+    .overlay .sale {
+        background-color: rgb(230, 36, 68);
+        color: #fff !important;
+    }
+    
+    .overlay .modal p {
+        font-weight: 600;
+        font-size: 18px;
+        padding-top: 20px;
+    }
+</style>
